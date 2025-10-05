f82fb0e4b0d5cf136b3cb122346b0b6d45031450
Xuzhong Wang <xwang58@wm.edu>
2025-10-05 14:42:07 -0400

    Update data structure skeleton

---
+++ graph_like.py
@@ -0,0 +1,6 @@
+from typing import Protocol, Iterable, Hashable
+
+class GraphLike(Protocol):
+    def nodes(self) -> Iterable[Hashable]: ...
+
+    def neighbors(self, v: Hashable) -> Iterable[Hashable]: ...
